e20ce827728d460f4eb8b4a93d0322e7e689594e
Stijn Nijland <stijn.nijland@hotmail.com>
2019-12-16 14:42:23 +0100

    beetje code

--- static/game.js
+++ static/game.js
@@ -1,7 +1,29 @@
 var socket = io();
-socket.on('message', function(data) {
-  console.log(data);
-});
+var canvas = document.getElementById('canvas');
+canvas.width = 640;
+canvas.height = 640;
+var context = canvas.getContext('2d');
+
+const objects = [];
+class object {
+  constructor(name, x, y, width, height){
+    this.name = name;
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+  }
+  pushToArray(){
+    objects.push(this);
+    console.log(objects);
+  }
+  build(){
+    context.beginPath();
+    context.fillStyle = "black";
+    context.rect(this.x, this.y, this.width, this.height);
+    context.fill();
+  }
+}
 
 var movement = {
     up: false,
@@ -44,8 +66,12 @@ var movement = {
 
 socket.emit('new player');
 setInterval(function() {
-  socket.emit('movement', movement);
+  socket.emit('movement', movement, objects);
 }, 1000 / 60);
+
+socket.on('message', function(data) {
+  console.log(data);
+});
 
 //Functie die kijk op welke positie de muis staat, geeft x en y coordinaat terug
 function getCursorPosition(canvas, event){
@@ -66,10 +92,6 @@ setInterval(function() {
   };
 }, 16);
 
-var canvas = document.getElementById('canvas');
-canvas.width = 640;
-canvas.height = 640;
-var context = canvas.getContext('2d');
 socket.on('state', function(players, bullets) {
   context.clearRect(0, 0, 640, 640);
   context.fillStyle = 'green';
@@ -79,6 +101,11 @@ socket.on('state', function(players, bullets) {
     context.arc(player.x, player.y, 10, 0, 2 * Math.PI);
     context.fill();  
   }
+
+  for(i=0; i<objects.length; i++){
+    objects[i].build();
+  }
+
   for (var id in bullets) {
     var bullet = bullets[id];
     context.beginPath();
@@ -86,3 +113,9 @@ socket.on('state', function(players, bullets) {
     context.fill();
   }
 });
+
+//alle objecten die worden getekend.
+let wall1 = new object("wall1", 160, 0, 2, 30); wall1.pushToArray();
+let wall2 = new object("wall2", 160, 60, 2, 100); wall2.pushToArray();
+let wall3 = new object("wall3", 0, 160, 30, 2); wall3.pushToArray();
+let wall4 = new object("wall4", 60, 160, 100, 2); wall4.pushToArray();
