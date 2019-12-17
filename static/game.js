5423f98f1970b3bc4c90151f43a9437df7c6adb9
TimJohansonDev <timjohanson@me.com>
2019-12-17 09:53:41 +0100

    halve map

--- static/game.js
+++ static/game.js
@@ -115,7 +115,48 @@ socket.on('state', function(players, bullets) {
 });
 
 //alle objecten die worden getekend.
-let wall1 = new object("wall1", 160, 0, 2, 30); wall1.pushToArray();
-let wall2 = new object("wall2", 160, 60, 2, 100); wall2.pushToArray();
-let wall3 = new object("wall3", 0, 160, 30, 2); wall3.pushToArray();
-let wall4 = new object("wall4", 60, 160, 100, 2); wall4.pushToArray();
+let wall1 = new object("wall1", 160, 0, 2, 25); wall1.pushToArray();
+let wall2 = new object("wall2", 160, 55, 2, 105); wall2.pushToArray();
+let wall3 = new object("wall3", 0, 160, 25, 2); wall3.pushToArray();
+let wall4 = new object("wall4", 55, 160, 105, 2); wall4.pushToArray();
+let wall5 = new object("wall5", 160, 80, 80, 2); wall5.pushToArray();
+let wall6 = new object("wall6", 240, 80, 2, 25); wall6.pushToArray();
+let wall7 = new object("wall7", 240, 135, 2, 50); wall7.pushToArray();
+let wall45 = new object("wall45", 80, 215, 2, 25); wall45.pushToArray();
+let wall46 = new object("wall46", 80, 160, 2, 25); wall46.pushToArray();
+let wall47 = new object("wall47", 240, 215, 2, 25); wall47.pushToArray();
+let wall48 = new object("wall48", 80, 240, 160, 2); wall48.pushToArray();
+let wall8 = new object("wall8", 240, 160, 65, 2); wall8.pushToArray();
+let wall9 = new object("wall9", 335, 160, 65, 2); wall9.pushToArray();
+let wall10 = new object("wall10", 400, 0, 2, 25); wall10.pushToArray();
+let wall11 = new object("wall11", 400, 55, 2, 105); wall11.pushToArray();
+let wall12 = new object("wall12", 320, 0, 2, 80); wall12.pushToArray();
+let wall13 = new object("wall13", 400,160,25,2); wall13.pushToArray();
+let wall14 = new object("wall14", 455,160,105,2); wall14.pushToArray();
+let wall15 = new object("wall15", 560,135,2,185); wall15.pushToArray();
+let wall16 = new object("wall16", 560,0,2,105); wall16.pushToArray();
+let wall17 = new object("wall17", 560,240,25,2); wall17.pushToArray();
+let wall18 = new object("wall18", 615,240,25,2); wall18.pushToArray();
+//let wall19 = new object("wall19", 560,320,25,2); wall19.pushToArray();
+let wall20 = new object("wall20", 615,320,25,2); wall20.pushToArray();
+let wall21 = new object("wall21", 400,160,2,80); wall21.pushToArray();
+let wall22 = new object("wall22", 400,240,80,2); wall22.pushToArray();
+let wall23 = new object("wall23", 480,160,2,25); wall23.pushToArray();
+let wall24 = new object("wall24", 480,215,2,50); wall24.pushToArray();
+let wall25 = new object("wall25", 480,295,2,50); wall25.pushToArray();
+let wall26 = new object("wall26", 480,320,25,2); wall26.pushToArray();
+let wall27 = new object("wall27", 535,320,50,2); wall27.pushToArray();
+//let wall28 = new object("wall28", 480,320,2,25); wall28.pushToArray();
+let wall29 = new object("wall29", 480,375,2,25); wall29.pushToArray();
+let wall30 = new object("wall30", 480,400,80,2); wall30.pushToArray();
+let wall31 = new object("wall31", 560,400,2,25); wall31.pushToArray();
+let wall32 = new object("wall32", 560,455,2,25); wall32.pushToArray();
+let wall33 = new object("wall33", 615,480,25,2); wall33.pushToArray();
+let wall34 = new object("wall34", 480,480,105,2); wall34.pushToArray();
+let wall35 = new object("wall35", 480,480,2,105); wall35.pushToArray();
+let wall36 = new object("wall36", 480,615,2,25); wall36.pushToArray();
+
+xywh
+
+
+
