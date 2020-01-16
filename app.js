d355286bb4412f9ebf46e67e897657c55f7d09d1
luukdewildt <dewildtluuk@gmail.com>
2020-01-16 11:50:49 +0100

    Revert "Merge branch 'master' of https://github.com/maidenfreak/mmoProjectGroepS4D"
    This reverts commit 0ab4ed9db1dbe7eb60c29592db65141819d05f05, reversing
    changes made to 9cfee9c2efcb8c6d7283f708e7bea5c51ab3fab8.

--- app.js
+++ app.js
@@ -151,8 +151,8 @@ class rebels extends character {
                 this.weapondamage = 30;
                 this.isDead = false;
                 this.score = 0;
-                this.maxAmmo = 25;
-                this.currentAmmo = 25;
+                this.maxAmmo = 20;
+                this.currentAmmo = 20;
                 this.classname = "Vigilante";
                 this.fireRate = 100;
             }        }
@@ -167,8 +167,8 @@ class rebels extends character {
                 this.weapondamage = 20;
                 this.isDead = false;
                 this.score = 0;
-                this.maxAmmo = 30;
-                this.currentAmmo = 30;
+                this.maxAmmo = 20;
+                this.currentAmmo = 20;
                 this.classname = "Separatist"; 
                 this.fireRate = 0;
             }        }
@@ -390,7 +390,6 @@ socket.on('disconnect', function(){
   console.log("lengte van object " + Object.entries(players).length)
   calculateWinner();
   }  
-
 });
 
 socket.on('leaveGame', function(){
@@ -421,7 +420,7 @@ socket.on('movement', function(data, objectArray) {
       player.x -= 2;
     }
   }
-  if (data.up && player.y>=11 && collision.checkCollisionUp(player, players, objectArray, 10) == false) {
+  if (data.up && player.y>=11 && collision.checkCollisionUp(player, players, objectArray, 9) == false) {
     var packageValues = collision.checkCollisionPackageUp(player, itemboxes , 9);
     if(packageValues[0] == true){
       addBoxItems(player, packageValues[1]);
@@ -432,7 +431,7 @@ socket.on('movement', function(data, objectArray) {
       player.y -= 2;
     }
   }
-  if (data.right && player.x<=630 && collision.checkCollisionRight(player, players, objectArray, 10) == false) {
+  if (data.right && player.x<=630 && collision.checkCollisionRight(player, players, objectArray, 9) == false) {
     var packageValues = collision.checkCollisionPackageRight(player, itemboxes , 9);
     if(packageValues[0] == true){
       addBoxItems(player, packageValues[1]);
@@ -443,7 +442,7 @@ socket.on('movement', function(data, objectArray) {
       player.x += 2;
     }
   }
-  if (data.down && player.y<=630 && collision.checkCollisionDown(player, players, objectArray, 10) == false) {
+  if (data.down && player.y<=630 && collision.checkCollisionDown(player, players, objectArray, 9) == false) {
     var packageValues = collision.checkCollisionPackageDown(player, itemboxes , 9);
     if(packageValues[0] == true){
       addBoxItems(player, packageValues[1]);
@@ -594,7 +593,7 @@ function updateHighscore(player){
     MongoClient.connect(url, function(err, db) {
       if (err) throw err;
       var dbo = db.db("mmodb");
-      var sort = { highscore: -1, winscore: -1 };
+      var sort = { highscore: 1, winscore: 1 };
 
       dbo.collection("highscoretable12").find().sort(sort).toArray(function(err, result) {
         for(var x in result){
